031eecb0b9bf65ae9e360b038bb0dcc843c45702
ChalitaK24 <chalurga@gmail.com>
2024-08-25 17:31:08 -0700

    shell 0

--- simple_shell0.c
+++ simple_shell0.c
@@ -7,6 +7,59 @@ extern char **environ;
 
 int main(void)
 {
+	char *in_line = NULL;
+	size_t ln = 0;
+	int nread;
+	pid_t ch_pid;
+	int status;
+
+	while (1)
+	{
+		printf("s_shell0$ ");
+
+		nread = getline(in_line, &ln, stdin);
+
+		if (nread == -1)
+		{
+			perror("Error: getline");
+			return (1);
+		}
+	}
+
+	if (in_line[nread - 1] == '\n')
+	{
+		in_line[nread - 1] = '\0';
+	}
+
+	pid = fork();
+	
+	if (pid == -1)
+	{
+		perror("Error: fork");
+		return (1);
+	}
+	else if (pid == 0)
+	{
+		char *argv[2];
+		argv[0] = in_line;
+		argv[1] = NULL;
+
+		if (execve(in_line, argv, environ) == -1)
+		{
+			perror(" ");
+		}
+	}
+	else
+	{
+		wait(NULL);
+
+		if (wait(status) == -1)
+		{
+			perror(" w");
+		}
+	}
+	free(in_line);
+	return(0);
+
 
 }
-
