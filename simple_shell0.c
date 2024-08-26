de55292f16258451b85935aa4b9741833e46b166
ChalitaK24 <chalurga@gmail.com>
2024-08-25 17:43:34 -0700

    shell ee

--- simple_shell0.c
+++ simple_shell0.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
 
 extern char **environ;
 
@@ -9,7 +11,7 @@ int main(void)
 {
 	char *in_line = NULL;
 	size_t ln = 0;
-	int nread;
+	ssize_t nread;
 	pid_t ch_pid;
 	int status;
 
@@ -31,14 +33,14 @@ int main(void)
 		in_line[nread - 1] = '\0';
 	}
 
-	pid = fork();
+	ch_pid = fork();
 	
-	if (pid == -1)
+	if (ch_pid == -1)
 	{
 		perror("Error: fork");
 		return (1);
 	}
-	else if (pid == 0)
+	else if (ch_pid == 0)
 	{
 		char *argv[2];
 		argv[0] = in_line;
@@ -53,7 +55,7 @@ int main(void)
 	{
 		wait(NULL);
 
-		if (wait(status) == -1)
+		if (wait(&status) == -1)
 		{
 			perror(" w");
 		}
